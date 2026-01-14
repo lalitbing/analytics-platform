623c88eab4b02b90cf218b6353e6e3473d0f24ad
Lalit Sharma <lalit9013@gmail.com>
2026-01-14 21:52:34 +0530

    prepare backend for production

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -5,12 +5,15 @@ dotenv.config({
   path: path.resolve(__dirname, "../.env")
 })
 
-console.log("CWD:", process.cwd())
-console.log("REDIS:", process.env.REDIS_URL)
-
 import app from "./app"
+import cors from "cors";
 
 const PORT = process.env.PORT || 4000
+
+app.use(cors({
+  origin: "*", // we will lock later
+  methods: ["GET","POST"]
+}));
 
 app.listen(PORT, () => {
   console.log(`API running on port ${PORT}`)
